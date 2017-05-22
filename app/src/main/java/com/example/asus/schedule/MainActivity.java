a1e13d3612eb517ad67296a334c25b7535647f58
Olga Kiseleva <kiseleva-1996@yandex.ru>
2017-05-22 13:38:39 +0300

    added first activity

--- app/src/main/java/com/example/asus/schedule/MainActivity.java
+++ app/src/main/java/com/example/asus/schedule/MainActivity.java
@@ -2,6 +2,13 @@ package com.example.asus.schedule;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import static android.R.attr.data;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        //String[] type_of_people = {"студент","преподаватель"};
+        // адаптер
+        ArrayAdapter<?> adapter = ArrayAdapter.createFromResource(this, R.array.type_of_people,android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        spinner.setAdapter(adapter);
+        // заголовок
+        spinner.setPrompt("Кто же ты?)");
+        // по умолчанию в этом элементе показывается 1 элемент (в нашем случае это студент)
+       // spinner.setSelection(1);
+        // устанавливаем обработчик нажатия*/
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view,
+                                       int position, long id) {
+                // показываем позиция нажатого элемента
+                Toast.makeText(getBaseContext(), "Твой выбор: = " + position, Toast.LENGTH_SHORT).show();
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> arg0) {
+            }
+        });
     }
 }
